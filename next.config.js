67f6f74fea283910fa50812d21ab053380dafb60
chrisgilbert722 <chrisgilbert722@gmail.com>
2025-11-05 11:50:45 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  // ✅ Ensure static files from /public are included
+  output: "standalone",
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
